112ec17b5b631423d1e2a44e151057212dc82246
Ray Arvin Rimorin <ray@fbmsoftware.com>
2024-02-05 03:22:36 +0800

    test dev api calls (production requires cors setup)

---
+++ src/ServiceLoader.jsx
@@ -0,0 +1,44 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { CircularProgress } from '@mui/material';
+import Axios from 'axios';
+import toast from 'react-hot-toast';
+import ReactJson from 'react-json-view';
+
+const ServiceLoader = () => {
+
+  const [loading, setLoading] = useState(true);
+
+  const [data, setData] = useState({});
+
+  const fetchData = useCallback(
+    async () => {
+      try {
+        const result = await Axios.get("https://dev.api.zennya.com/api/4/services?latitude=14.566065988336462&longitude=120.98191450465623&device_type=WEB")
+        setData(result.data);
+      } catch (error) {
+        toast.error(error?.message?.en || error?.response?.data?.message?.en || error?.response?.data?.details || error?.message)
+      } finally {
+        setLoading(false);
+      }
+    }, []
+  );
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  if (loading) {
+    return (
+      <CircularProgress />
+    );
+  }
+  return (
+    <ReactJson
+      src={{ data }}
+      collapsed={2}
+    />
+  )
+}
+
+export { ServiceLoader };
